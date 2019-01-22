a51fc1a92c8543e73edada886ed46c49f636a8b3
ysje1010235 <you@example.comysje1010235@naver.com>
2019-01-22 18:12:03 +0900

    0122

---
+++ js/main2.js
@@ -0,0 +1,16 @@
+/**** var n = 1	;
+var interval = setInterval(ani, 500); //애니실행속도
+
+$(".doctor").each(function(i){
+	$(this).css({"left":(i*100)+"%"});
+});
+
+function ani(){
+	$(".doctors").stop().animate({"left":(-n*100)+"%"}, 5000, function(){ //애니속도
+	});
+} ****/
+
+$(".doctor").animate({
+
+	margin
+}1500.)
